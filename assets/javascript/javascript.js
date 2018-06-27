7d6e7b18c4887f110442719b3d6060daf7dcd1c7
Yeiska <yeiska83@yahoo.com>
2018-06-27 18:53:44 -0400

    version 3

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -7,7 +7,7 @@ function displayAnimalInfo() {
   var topic = $(this).attr("data-name");
   console.log(topic);
   var key = "dc6zaTOxFJmzC";
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + key + "&limit=10";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + key + "&limit=10";
 
   console.log(queryURL);
 
